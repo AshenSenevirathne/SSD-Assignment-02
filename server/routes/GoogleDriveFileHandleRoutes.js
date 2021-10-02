a32da6c2c1cee6e91857c273ff3b09721de6e1f4
Kusal Priyanka Perera <50095317+KusalPriyanka@users.noreply.github.com>
2021-10-02 20:40:43 +0530

    Implement multer functions

--- server/routes/GoogleDriveFileHandleRoutes.js
+++ server/routes/GoogleDriveFileHandleRoutes.js
@@ -4,4 +4,17 @@ const multer = require("multer");
 const fs = require("fs");
 const oAuth2Client = require("../services/oAuth2Client");
 
+var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./images");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+var upload = multer({
+  storage: Storage,
+}).single("file");
+
 module.exports = router;
